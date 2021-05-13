c7f6186fff61e69ea7954bc6193ee999e73d3c78
hahoho87 <hahoho87@gmail.com>
2021-05-14 08:42:50 +0900

    feat : 잔돈 반환 및 누적 기능 구현

--- src/test/java/vendingmachine/VendingMachineTest.java
+++ src/test/java/vendingmachine/VendingMachineTest.java
@@ -1,4 +1,40 @@
-import static org.junit.jupiter.api.Assertions.*;
+package vendingmachine;
+
+import com.sun.org.glassfish.gmbal.ParameterNames;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class VendingMachineTest {
-  
+
+
+    @Test
+    @DisplayName("자판기 생성")
+    void createVendingMachine() {
+        VendingMachine vendingMachine = new VendingMachine();
+        assertThat(vendingMachine).isNotNull();
+    }
+
+    @ParameterizedTest(name = "자판기에 {0} 원을 넣으면 {0}원이 들어있음을 알 수 있다")
+    @ValueSource(ints = {500, 1000})
+    @DisplayName("자판기에 {0} 원을 넣으면 {0}원이 들어있음을 알 수 있다")
+    void returnXWhenInputX(int changes) {
+        VendingMachine vendingMachine = new VendingMachine();
+        vendingMachine.put(changes);
+        assertThat(vendingMachine.getChanges()).isEqualTo(changes);
+    }
+
+    @Test
+    @DisplayName("1000원이 들어있는 자판기에 500원을 넣으면 500원이 들어있는 것을 알 수 있다")
+    void return500WhenPut500InVendingMachineWith1000() {
+        VendingMachine vendingMachine = new VendingMachine();
+        int baseMoney = 1000;
+        int inputMoney = 500;
+        vendingMachine.put(baseMoney);
+        vendingMachine.put(inputMoney);
+        assertThat(vendingMachine.getChanges()).isEqualTo(1500);
+    }
 }
