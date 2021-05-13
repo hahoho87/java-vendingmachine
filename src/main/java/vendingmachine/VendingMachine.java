29d0aea5fb202234bb9ac509aa392db1df15aad1
hahoho87 <hahoho87@gmail.com>
2021-05-14 08:42:50 +0900

    feat : 잔돈 반환 및 누적 기능 구현

--- src/main/java/vendingmachine/VendingMachine.java
+++ src/main/java/vendingmachine/VendingMachine.java
@@ -1,2 +1,14 @@
-package vendingmachine;public class VendingMachine {
+package vendingmachine;
+
+public class VendingMachine {
+
+    private int changes;
+
+    public void put(final int inputMoney) {
+        changes += inputMoney;
+    }
+
+    public int getChanges() {
+        return this.changes;
+    }
 }
